33244aa44e3586a541be41c2c468a21b6793c95b
Arthur Latimier <arthur.latimier@gmail.com>
2019-06-13 01:15:07 +0200

    Implement optimized packager

---
+++ src/main/packager/optimizedPackager.js
@@ -0,0 +1,24 @@
+const {Box} = require("../model/box");
+
+class OptimizedPackager {
+
+    pact(articles) {
+        const boxes = [];
+        articles.forEach(article => {
+            let availableBox = this.findFirstAvailableBox(boxes, article);
+            if (!availableBox) {
+                availableBox = new Box();
+                boxes.push(availableBox);
+            }
+            availableBox.addArticle(article);
+        });
+        return boxes;
+    }
+
+    findFirstAvailableBox(boxes, article) {
+        return boxes.find(b => b.getAvailableSpace() >= article)
+    }
+
+}
+
+module.exports = {OptimizedPackager};
